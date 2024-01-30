e2ea50c153d65132fb7658fc14f5b38c3f7e3402
Emyjakarta <ukonud4@gmail.com>
2024-01-30 23:25:05 +0100

    task 1

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 # A script to match lines having 2 to 5 't' in the string starting
 # with 'hb' and ending with 'n'
-regex = /hbt*n/
+regex = /hbt{2,5}n/
 
 ARGV.each do |arg|
   match = arg.scan(regex).join("\n")
