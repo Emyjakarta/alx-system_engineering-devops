7834a1f211e0eed569f2916cf06a2543b2e66480
Emyjakarta <ukonud4@gmail.com>
2024-01-30 23:33:03 +0100

    task 2

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
+# A script that matches the lines with 1 or 2
+# alphabets inside the string "hn"
 
 regex = /h[bt]*n/
-
 ARGV.each do |arg|
   match = arg.scan(regex).join
   puts match
