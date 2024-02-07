1285a0861c05b771e8c53f9c35f6bd92c73f4723
Decency Chukwuemeka Ukonu <141342695+Emyjakarta@users.noreply.github.com>
2024-02-07 10:27:45 +0100

    Delete 0x06-regular_expressions directory

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-
-regex = /hbt+n/
-
-ARGV.each do |arg|
-  match = arg.scan(regex).join
-  puts match
-end    
--- 0x06-regular_expressions/5-beginning_and_end.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-
-regex = /^h.n$/
-
-ARGV.each do |arg|
-  match = arg.scan(regex).join
-  puts match
-end    
