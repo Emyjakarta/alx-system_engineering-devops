71b6658e93596788cae1dc07d7031a166cf12b08
Emyjakarta <ukonud4@gmail.com>
2024-05-04 14:11:38 +0100

    added comments

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,3 +1,5 @@
+# manifest that configures the SSH client to use the SSH key for the user
+
 file_line { 'Turn off passwd auth':
   path   => '/etc/ssh/ssh_config',
     line => 'PasswordAuthentication no',
