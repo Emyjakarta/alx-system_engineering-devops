366cf8df07158e30a96655989f244ec147de0508
Emyjakarta <ukonud4@gmail.com>
2024-05-14 00:41:47 +0100

    corrected advanced task

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,7 +1,11 @@
+# Install the puppetlabs-stdlib module
+class { 'stdlib': }
+
 # Update package information
 exec { 'apt-update':
-  command => '/usr/bin/apt-get -y update',
-    path  => ['/usr/bin', '/bin'],
+  command  => '/usr/bin/apt-get -y update',
+    path   => ['/usr/bin', '/bin'],
+    before => Package['nginx'],
 }
 
 # Install Nginx package
@@ -10,14 +14,23 @@ exec { 'apt-update':
 }
 
 # Configure Nginx to add the custom HTTP header
-      file_line { 'add custom header':
-        ensure => present,
-    path       => '/etc/nginx/sites-available/default',
-      line     => "\tadd_header X-Served-By ${hostname};",
-        after  => 'server_name _;',
+#      file_line { 'add custom header':
+#        ensure => present,
+#    path       => '/etc/nginx/sites-available/default',
+#      line     => "\tadd_header X-Served-By ${hostname};",
+#        after  => 'server_name _;',
+#}
+
+# Configure Nginx to add the custom HTTP header
+file { '/etc/nginx/sites-available/default':
+  ensure  => present,
+  content => template('nginx/default.erb'),
+  require => Package['nginx'],
+  notify  => Service['nginx'],
 }
 
 # Ensure Nginx is running
         service { 'nginx':
           ensure => running,
+    enable       => true,
 }
