4cf3a142638ede9a177125f68190b04a74528ac5
Emyjakarta <ukonud4@gmail.com>
2024-06-04 01:05:31 +0100

    Using what you did in the task #0, extend your Python script to export data in the CSV format

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+Module that provides a function to export user tasks to a CSV file.
+"""
+
+import custom_methods
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.stderr.write(f"Usage: {sys.argv[0]} <user_id>\n")
+        sys.exit(1)
+
+    user_id = sys.argv[1]
+    user = custom_methods.retrieve_username(user_id)
+    if user is None:
+        sys.stderr.write("Invalid user id.\n")
+        sys.exit(1)
+
+    todos = custom_methods.retrieve_todos(user_id)
+    custom_methods.export_to_csv(user_id=user_id, username=user, tasks=todos)
